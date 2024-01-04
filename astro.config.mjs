9c8d882fcda638f38aadbd2055f3400fcb3fc754
Alex Ledger <alex@playgroundlabs.io>
2024-01-04 15:05:34 -0800

    Set up svelte

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,21 +1,19 @@
 import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
-
 import mdx from "@astrojs/mdx";
+
+import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
   markdown: {
-    smartypants: false,
+    smartypants: false
   },
   build: {
-    format: "file",
+    format: "file"
   },
   site: "https://alexledger.net",
-  integrations: [
-    sitemap({
-      // filter: (page) => page !== 'https://stargazers.club/secret-vip-lounge/',
-    }),
-    mdx(),
-  ],
+  integrations: [sitemap({
+    // filter: (page) => page !== 'https://stargazers.club/secret-vip-lounge/',
+  }), mdx(), svelte()]
 });
