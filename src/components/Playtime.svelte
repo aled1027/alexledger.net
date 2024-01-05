189e19604a6968b3fcce0cf86abfac103025b2c8
Alex Ledger <alex@playgroundlabs.io>
2024-01-04 16:44:00 -0800

    Updates

--- src/components/Playtime.svelte
+++ src/components/Playtime.svelte
@@ -5,10 +5,11 @@
   function init() {
     // set the dimensions and margins of the graph
     const margin = { top: 10, right: 30, bottom: 30, left: 60 },
-      width = 1080 - margin.left - margin.right,
-      height = 400 - margin.top - margin.bottom;
+      width = window.innerWidth - margin.left - margin.right,
+      height = window.innerHeight / 2 - margin.top - margin.bottom;
 
     // append the svg object to the body of the page
+    // Put it outside of the data callback so that it can be updated
     const svg = d3
       .select("#my_dataviz")
       .append("svg")
@@ -25,29 +26,51 @@
       function (d) {
         return {
           date: d3.timeParse("%Y-%m-%d")(d.date),
+
+          // adding some noise to the data
           value: parseFloat(d.value) + Math.random() * 4000,
         };
       }
     ).then(
       // Now I can use this dataset:
       function (data) {
-        console.log(data);
-
-        const yMin = d3.min(data.map((item) => parseInt(item.value))) * 0.8;
-        const yMax = d3.max(data.map((item) => parseInt(item.value))) * 1.1;
+        data = data.slice(0, 100); // limit the data
 
         // Add X axis --> it is a date format
         const x = d3
           .scaleTime()
           .domain(d3.extent(data, (d) => d.date))
           .range([0, width]);
+
+        // Add the bottom axis
         svg
           .append("g")
           .attr("transform", "translate(0," + height + ")")
           .call(d3.axisBottom(x));
+
         // Add Y axis
-        const y = d3.scaleLinear().domain([yMin, yMax]).range([height, 0]);
+        const ys = data.map((item) => item.value);
+        const y = d3
+          .scaleLinear()
+          .domain([d3.min(ys), d3.max(ys)])
+          .range([height, 0]);
+
+        // Add the left axis
         svg.append("g").call(d3.axisLeft(y));
+
+        // Add the points
+        svg
+          .append("g")
+          .selectAll("dot")
+          .data(data)
+          .join("circle") // This might be the old way? not sure
+          .attr("cx", (d) => x(d.date))
+          .attr("cy", (d) => y(d.value))
+          .attr("r", 5)
+          .attr("fill", "#69b3a2")
+          .append("svg:title") // adds a browser-supported mouse-over with title
+          .text((d) => `${d.date.toLocaleDateString()}: ${d.value}`);
+
         // Add the line
         svg
           .append("path")
@@ -62,16 +85,6 @@
               .x((d) => x(d.date))
               .y((d) => y(d.value))
           );
-        // Add the points
-        svg
-          .append("g")
-          .selectAll("dot")
-          .data(data)
-          .join("circle")
-          .attr("cx", (d) => x(d.date))
-          .attr("cy", (d) => y(d.value))
-          .attr("r", 5)
-          .attr("fill", "#69b3a2");
       }
     );
   }
