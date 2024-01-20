cd6ab9fb747de963afb1518b5a7a16d76b45e13b
Alex Ledger <alex@playgroundlabs.io>
2024-01-20 10:24:16 -0800

    Move spotlight to lists.

--- tina/config.ts
+++ tina/config.ts
@@ -1,5 +1,32 @@
 import { defineConfig } from "tinacms";
 
+type ObjectField = {
+  label: string
+  name: string
+  type: 'object'
+  list?: boolean
+  /** `fields OR `templates` may be provided, not both **/
+  fields?: Field[]
+  templates?: Template[]
+  /** Customize the default "_template" key that gets set
+     in a document to identify a block-type.
+     Only applicable when list: true **/
+  templatesKey?: string
+  list?: boolean
+  /** See https://tina.io/docs/extending-tina/overview/ for customizing the UI **/
+  ui?: {
+     /** Weather or not the Visual Selector  is enabled. See https://tina.io/docs/editing/blocks/#adding-a-visual-block-selector-experimental **/
+    visualSelector?: boolean,
+    // Note: defaultItem can only can be used when {list: true}
+    defaultItem?: Record<string, any> | () => Record<string, any>,
+    itemProps?(
+      item: Record<string, any>
+    ): {
+      label?: string
+    }
+  }
+}
+
 // Your hosting provider likely exposes this as an environment variable
 const branch =
   process.env.GITHUB_BRANCH ||
@@ -91,6 +118,74 @@ export default defineConfig({
             label: "Body",
             isBody: true,
           },
+        ],
+      },
+      {
+        name: "groceries",
+        label: "Groceries",
+        path: "src/content/groceries",
+        fields: [
+          {
+            type: "string",
+            name: "name",
+            label: "Name",
+            isTitle: true,
+            required: true,
+          },
+          {
+            type: "string",
+            name: "currentOwner",
+            label: "Current Owner",
+            required: true,
+          },
+          {
+            type: "rich-text",
+            name: "body",
+            label: "Body",
+            isBody: true,
+          },
+          {
+            label: "Flags",
+            name: "flags",
+            type: "object",
+            list: true,
+            ui: {
+              itemProps: (item) => {
+                return { label: `${item?.label}  (${item?.greenyellowred}) `}
+              },
+            },
+            fields: [
+              {
+                label: "Label",
+                name: "label",
+                type: "string"
+              },
+              {
+                label: "GreenYellowRed",
+                name: "greenyellowred",
+                type: "string",
+                options: [
+                  {
+                    value: "green",
+                    label:"Green",
+                  },
+                  {
+                    value: "yellow",
+                    label:"Yellow",
+                  },
+                  {
+                    value: "red",
+                    label:"Red",
+                  }
+                ]
+              },
+              {
+                label: "Description",
+                name: "description",
+                type: "rich-text",
+              }
+            ]
+          }
         ],
       },
     ],
