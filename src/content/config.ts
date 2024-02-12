60336868e5d1603bbcd3f45b41f81e3441307681
Alex Ledger <alex@playgroundlabs.io>
2024-02-12 12:00:36 -0800

    Add ferments section

--- src/content/config.ts
+++ src/content/config.ts
@@ -16,7 +16,19 @@ export const collections = {
       title: z.string(),
       date: z.coerce.date(),
       includeToc: z.boolean().default(true),
+      tags: z.array(z.string()).default([]),
       // description: z.string().max(200),
+    }),
+  }),
+  ferments: defineCollection({
+    schema: z.object({
+      title: z.string(),
+      date: z.coerce.date(),
+      includeToc: z.boolean().default(false),
+      dateStart: z.optional(z.date()),
+      dateEnd: z.optional(z.date()),
+      status: z.string(),
+      fermentType: z.string(),
     }),
   }),
 };
