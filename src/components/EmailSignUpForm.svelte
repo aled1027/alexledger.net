bb84e6801bae668f8cb80762aa4234da47da523e
Alex Ledger <alex@playgroundlabs.io>
2024-02-29 13:37:34 -0800

    Update

--- src/components/EmailSignUpForm.svelte
+++ src/components/EmailSignUpForm.svelte
@@ -1,25 +1,40 @@
 <script>
 </script>
 
-<link
-  rel="stylesheet"
-  href="https://cdn.jsdelivr.net/npm/modern-css-reset/dist/reset.min.css"
-/>
-
 <div class="container">
-  <form action="" method="POST">
+  <form action="" method="POST" class="signup__form">
     <div class="form-inputs">
       <label for="email">Email Address</label>
       <input
-        id="email"
-        name="email"
         type="email"
-        placeholder="Your email address"
+        name="email"
+        id="email"
+        autocapitalize="none"
+        autocorrect="none"
         required
+        pattern="[^@]+@[^\.]+\..+"
       />
     </div>
-    <button type="submit"><span>-</span></button>
+    <button type="submit">
+      <!-- This pattern is from the piccalil solution -->
+      <span class="visually-hidden">Submit email</span>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        aria-hidden="true"
+        focusable="false"
+        width="1em"
+        height="1em"
+        viewBox="0 0 24 24"
+      >
+        <path
+          fill="currentColor"
+          d="M11.293 5.707L16.586 11H5a1 1 0 000 2h11.586l-5.293 5.293a.999.999 0 101.414 1.414l7-7a1.006 1.006 0 000-1.414l-7-7a.999.999 0 10-1.414 1.414z"
+        />
+      </svg></button
+    >
   </form>
+  <!-- TODO: is this used? -->
+  <div aria-atomic="true" role="alert" class="signup__alert"></div>
 </div>
 
 <style>
@@ -38,9 +53,12 @@
   .container {
     margin: 4rem 2rem;
     padding: 2rem;
+    display: grid;
+    place-items: center;
+    border: 1px solid var(--color-primary);
   }
 
-  form {
+  .signup__form {
     display: flex;
     place-items: end;
   }
@@ -68,6 +86,7 @@
     height: 2.75rem;
     width: 2.75rem;
     background: var(--color-secondary);
+    font-size: 1.625rem;
     color: var(--color-primary);
     border-radius: 0;
     border: none;
@@ -76,5 +95,21 @@
     display: flex;
     justify-content: center;
     place-items: center;
+  }
+
+  /**
+ * VISUALLY HIDDEN UTILITY
+ */
+  .visually-hidden {
+    /* Copied from https://piccalil.li/tutorial/solution-001-email-sign-up-form/ */
+    border: 0;
+    clip: rect(0 0 0 0);
+    height: auto;
+    margin: 0;
+    overflow: hidden;
+    padding: 0;
+    position: absolute;
+    width: 1px;
+    white-space: nowrap;
   }
 </style>
