7877e8a761be92270d36397578b5d6636788f882
Alex Ledger <alex@playgroundlabs.io>
2024-10-03 13:15:10 -0700

    Looking pretty okay

--- src/components/ProjectCard.svelte
+++ src/components/ProjectCard.svelte
@@ -1,13 +1,16 @@
 <script lang="ts">
-  let projectName = "inContext";
-  let description =
-    "inContext is a language-learning browser extension that builds tailored, context-informed definitions of words and displays them in the webpage.";
-  let url = "https://incontext.fun";
-  let videoUrl =
-    "https://www.youtube.com/embed/rZlKI_I89ZI?showinfo=0&autoplay=1&mute=1&loop=1&modestbranding=1&rel=0&iv_load_policy=3&fs=0&controls=0&disablekb=1&playlist=rZlKI_I89ZI";
-  //   let videoUrl = "";
+  interface Project {
+    projectName: string;
+    description: string;
+    url: string;
+    videoUrl: string;
+    label: string;
+  }
+  export let project: Project;
 
   // TODO: figure out what goes in the label section
+  // TODO: on hover, could do a diagonal animation sheen across the whole card, in the background
+  // TODO: could incorporate the brand colors somewhere
 
   let label = "";
 
@@ -17,24 +20,24 @@
 <div class="project-card">
   <div class="project-card__title-wrapper">
     <h3 class="heading-3">
-      <a class="text-decoration-none" href={url} target="_blank"
-        >{projectName}</a
+      <a class="text-decoration-none" href={project.url} target="_blank"
+        >{project.projectName}</a
       >
     </h3>
   </div>
   <div class="project-card__middle-entry">
     <p class="">
-      {description}
+      {project.description}
     </p>
   </div>
-  {#if videoUrl}
+  {#if project.videoUrl}
     <div class=".project-card__middle-entry padding-4">
       <div class="project-card__video-wrapper">
         <iframe
-          src={videoUrl}
+          src={project.videoUrl}
           width="100%"
           height="100%"
-          title="{projectName} Video"
+          title="{project.projectName} Video"
           frameborder="0"
         ></iframe>
       </div>
@@ -42,8 +45,8 @@
   {/if}
   <div class="project-card__bottom">
     <div class="project-card__bottom-left">
-      <a href={url}>Visit site</a>
+      <a href={project.url}>Visit site</a>
     </div>
-    <div class="project-card__bottom-right">{label}</div>
+    <div class="project-card__bottom-right">{project.label}</div>
   </div>
 </div>
--- src/components/ProjectsWithCards.svelte
+++ src/components/ProjectsWithCards.svelte
@@ -1,11 +1,35 @@
 <script lang="ts">
   import ProjectCard from "./ProjectCard.svelte";
+
+  let projects = [
+    {
+      projectName: "inContext",
+      description:
+        "inContext is a language-learning browser extension that builds tailored, context-informed definitions of words and displays them in the webpage.",
+      url: "https://incontext.fun",
+      videoUrl:
+        "https://www.youtube.com/embed/rZlKI_I89ZI?showinfo=0&autoplay=1&mute=1&loop=1&modestbranding=1&rel=0&iv_load_policy=3&fs=0&controls=0&disablekb=1&playlist=rZlKI_I89ZI",
+      label: "",
+    },
+    {
+      projectName: "Compass",
+      description:
+        "An app for building guides for family & friends when they visit cities that you know, and you want to share your favorite spots.",
+      url: "https://thelittlecompass.com",
+      videoUrl: "",
+      label: "",
+    },
+  ];
 </script>
 
 <h2 class="heading-2">Projects</h2>
 
-<div class="project-cards">
-  <ProjectCard />
+<div class="project-cards masonry">
+  {#each projects as project}
+    <div class="masonry-item">
+      <ProjectCard {project} />
+    </div>
+  {/each}
 </div>
 
 <style>
