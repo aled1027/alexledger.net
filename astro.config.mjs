ea45183286dac4c8db31cece3691309aff2a9aab
Alex Ledger <alex@playgroundlabs.io>
2024-10-04 15:10:34 -0700

    Update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,4 @@
-import { defineConfig } from "astro/config";
+import { defineConfig, passthroughImageService } from "astro/config";
 import sitemap from "@astrojs/sitemap";
 import mdx from "@astrojs/mdx";
 import svelte from "@astrojs/svelte";
@@ -7,6 +7,12 @@ import lit from "@astrojs/lit";
 
 // https://astro.build/config
 export default defineConfig({
+  image: {
+    // After updating astro, I started getting the error:
+    // "title: 'Could not find Sharp.'". According to the docs,
+    // this disables Sharp, the image processor.
+    service: passthroughImageService(),
+  },
   markdown: {
     // https://docs.astro.build/en/guides/markdown-content/
     shikiConfig: {
