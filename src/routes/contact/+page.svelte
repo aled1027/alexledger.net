13b318d04214a3a4d33b249efebf9adbba208987
Alex Ledger <alex@playgroundlabs.io>
2025-03-31 14:30:06 -0700

    Update +page.svelte

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -8,18 +8,22 @@
 		You can book an initial call by filling out the form below or emailing me at
 		hello@alexledger.net.
 	</p>
-	<form class="mt-l" name="contact" data-netlify="true">
+	<form class="mt-l" name="contact" method="POST" data-netlify="true" netlify-honeypot="bot-field">
+		<input type="hidden" name="form-name" value="contact" />
+		<p class="hidden">
+			<label>Don't fill this out if you're human: <input name="bot-field" /></label>
+		</p>
 		<div class="input-group">
 			<label for="name">Name</label>
-			<input type="text" id="name" name="name" />
+			<input type="text" id="name" name="name" required />
 		</div>
 		<div class="input-group">
 			<label for="email">Email</label>
-			<input type="email" id="email" name="email" />
+			<input type="email" id="email" name="email" required />
 		</div>
 		<div class="input-group">
 			<label for="message">Message</label>
-			<textarea id="message" name="message"></textarea>
+			<textarea id="message" name="message" required></textarea>
 		</div>
 		<button class="button mt-m" type="submit">Submit</button>
 	</form>
@@ -36,5 +40,9 @@
 		display: flex;
 		flex-direction: column;
 		gap: var(--space-2xs);
+	}
+
+	.hidden {
+		display: none;
 	}
 </style>
