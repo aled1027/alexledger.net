e73fbf5e2e430557faf5c8f4a8bd7deee9620182
Alex Ledger <alex@playgroundlabs.io>
2025-03-31 14:27:15 -0700

    Add 404 and try to ssg

---
+++ src/routes/+error.svelte
@@ -0,0 +1,12 @@
+<section class="about" data-narrow-main>
+	<h1>Not found</h1>
+	<p>Sorry, the page you're looking for doesn't exist.</p>
+	<ul>
+		<li><a href="/">Go to the homepage</a></li>
+		<li><a href="/contact">Contact me</a></li>
+		<li><a href="/about">About me</a></li>
+	</ul>
+</section>
+
+<style>
+</style>
