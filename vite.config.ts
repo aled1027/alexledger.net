79984ef5aac84813cf3cf41e37860604189a68d8
Alex Ledger <alex@playgroundlabs.io>
2025-06-26 11:54:09 -0700

    Add devToolsJson

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 import { enhancedImages } from '@sveltejs/enhanced-img';
-
+import devtoolsJson from 'vite-plugin-devtools-json';
 
 export default defineConfig({
-	plugins: [enhancedImages(), sveltekit()],
+	plugins: [enhancedImages(), sveltekit(), devtoolsJson()],
 
 	// See https://vite.dev/config/shared-options#css-preprocessoroptions
 	css: {
