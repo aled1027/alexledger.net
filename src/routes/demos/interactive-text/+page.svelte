2404ec0b64f90906317077b2cce1438d92cb0a3a
Alex Ledger <alex@playgroundlabs.io>
2025-06-30 09:49:16 -0700

    Basic version working

--- src/routes/demos/interactive-text/+page.svelte
+++ src/routes/demos/interactive-text/+page.svelte
@@ -10,6 +10,7 @@
 	let pointer: THREE.Vector2;
 	let sphereMesh: THREE.Mesh;
 	let hitMesh: THREE.Mesh;
+	let exploreShaderMaterial: THREE.ShaderMaterial;
 
 	// Animation loop
 	function animate() {
@@ -35,6 +36,14 @@
 		const intersects = raycaster.intersectObjects([hitMesh]);
 		if (intersects.length > 0) {
 			sphereMesh.position.set(intersects[0].point.x, intersects[0].point.y, intersects[0].point.z);
+
+			exploreShaderMaterial.uniforms.uDisplacement.value.set(
+				intersects[0].point.x,
+				intersects[0].point.y,
+				intersects[0].point.z
+			);
+		} else {
+			exploreShaderMaterial.uniforms.uDisplacement.value.set(40, 40, 40);
 		}
 	}
 
@@ -56,15 +65,38 @@
 		const textureLoader = new THREE.TextureLoader();
 		const texture = textureLoader.load('/textures/explore.png');
 
-		const shaderMaterial = new THREE.ShaderMaterial({
+		exploreShaderMaterial = new THREE.ShaderMaterial({
 			uniforms: {
-				uTexture: { value: texture }
+				uTexture: { value: texture },
+				uDisplacement: { value: new THREE.Vector3(0, 0, 0) }
 			},
 			vertexShader: `
 				varying vec2 vUv;
+				uniform vec3 uDisplacement;
+
+                float easeInOutCubic(float x) {
+                    return x < 0.5 ? 4. * x * x * x : 1. - pow(-2. * x + 2., 3.) / 2.;
+                }
+
+                float map(float value, float min1, float max1, float min2, float max2) {
+                    return min2 + (value - min1) * (max2 - min2) / (max1 - min1);
+                }
+
 				void main() {
 					vUv = uv;
-					gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
+                    vec3 new_position = position;
+
+                    vec4 localPosition = vec4( position, 1.);
+                    vec4 worldPosition = modelMatrix * localPosition;
+                    float dist = (length(uDisplacement - worldPosition.rgb));
+
+                    float min_distance = 3.;
+                    if (dist < min_distance) {
+                        float distance_mapped = map(dist, 0., min_distance, 1., 0.);
+                        float zDiff = easeInOutCubic(distance_mapped) * 1.; //1 is the max height of displacement
+                        new_position.z += zDiff;
+                    }
+                    gl_Position = projectionMatrix * modelViewMatrix * vec4(new_position,1.0);
 				}
 			`,
 			fragmentShader: `
@@ -79,7 +111,7 @@
 		});
 
 		const exploreGeometry = new THREE.PlaneGeometry(15, 15, 10, 10);
-		const exploreMesh = new THREE.Mesh(exploreGeometry, shaderMaterial);
+		const exploreMesh = new THREE.Mesh(exploreGeometry, exploreShaderMaterial);
 		exploreMesh.position.set(0, 0, 0);
 		exploreMesh.rotation.z = Math.PI / 4;
 		scene.add(exploreMesh);
