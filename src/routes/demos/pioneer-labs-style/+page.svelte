fc8b3a1d682fef035333b2d30017ffd074b682f3
Alex Ledger <alex@playgroundlabs.io>
2025-12-03 11:12:05 -0800

    Closer

--- src/routes/demos/pioneer-labs-style/+page.svelte
+++ src/routes/demos/pioneer-labs-style/+page.svelte
@@ -3,7 +3,6 @@
 	import ScrollTrigger from 'gsap/ScrollTrigger';
 	import { onMount } from 'svelte';
 
-	let container: HTMLDivElement;
 	let overlayEle: HTMLDivElement;
 	let overlayContentEle: HTMLDivElement;
 	let pioneerWrapper: HTMLDivElement; // Add this
@@ -19,12 +18,12 @@
 		gsap.to(overlayEle, {
 			ease: 'none',
 			scale: 1,
-			borderRadius: "0",
+			borderRadius: '0',
 			scrollTrigger: {
 				trigger: '.pioneer-wrapper',
 				start: '0vh top',
 				end: '+=300vh',
-				scrub: true,
+				scrub: true
 			}
 		});
 
@@ -37,36 +36,34 @@
 				trigger: '.pioneer-wrapper',
 				start: '200vh top',
 				end: '+=200vh',
-				scrub: true,
+				scrub: true
 			}
 		});
 
 		// Hide the overlay after animations complete so normal scrolling can continue
-		gsap.to(overlayEle, { // Change from overlayEle to pioneerWrapper
+		gsap.to(overlayEle, {
+			// Change from overlayEle to pioneerWrapper
 			ease: 'none',
 			y: '-100%',
 			scrollTrigger: {
 				trigger: '.pioneer-wrapper',
-				start: '400vh top',
-				end: '+=100vh',
-				scrub: true,
+				start: '500vh top',
+				end: '+=400vh',
+				scrub: true
 			}
 		});
 
-		gsap.set(".content1", {
+		gsap.set('.content1', {
 			scrollTrigger: {
 				trigger: '.pioneer-wrapper',
-				start: '400vh top',
+				start: '200vh top',
 				end: '+=1vh',
 				scrub: true,
-				markers: true,
 				onUpdate: (self) => {
-					gsap.set(".content1", { opacity: 1 - self.progress });
+					gsap.set('.content1', { opacity: 1 - self.progress });
 				}
 			}
 		});
-
-
 	});
 </script>
 
@@ -76,7 +73,7 @@
 			<br />
 			<br />
 			<h3>Space</h3>
-			<p>Your mission, should you choose to accept it, is to engineer microbes for Mars. </p>
+			<p>Your mission, should you choose to accept it, is to engineer microbes for Mars.</p>
 		</div>
 	</div>
 	<div class="content1">
@@ -85,20 +82,18 @@
 			<a href="https://www.pioneer-labs.org/" target="_blank">Pioneer Labs</a> scroll effect.
 		</p>
 		<p>Scroll down, if you please.</p>
-		<div class="" bind:this={container}></div>
 	</div>
 </div>
 
 <div class="content2">
-	<h2>And continue!</h2>
+	<h2>And more content!</h2>
 	<p>pretty fun, no?</p>
 </div>
 
 <style lang="scss">
 	.pioneer-wrapper {
 		position: relative;
-		min-height: 200vh;
-		padding-bottom: 50vh; // Optional: adds breathing room before content2
+		min-height: 150vh;
 	}
 
 	.content1 {
@@ -117,7 +112,7 @@
 		transform: translateX(-50%);
 		width: 100vw;
 		height: calc(100vh - 100px);
-		background: url("/textures/sky-base.jpg") no-repeat center center;
+		background: url('/textures/sky-base.jpg') no-repeat center center;
 		scale: 0;
 		transform-origin: bottom center;
 		border-top-left-radius: 80%;
