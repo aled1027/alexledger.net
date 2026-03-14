3177c3da41278210ddc09ef8b9c3799df9475597
Alex Ledger <alex@alexledger.net>
2026-03-14 09:17:33 -0700

    Revert "Remove menuMinRadius and adjust header clip-path"
    This reverts commit 02d0991b7881db9e1ad07f1b79df370bd02b69b2.

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -34,6 +34,7 @@
 	let menuCenterX = $state(0);
 	let menuCenterY = $state(0);
 	let menuRadius = $state(0);
+	let menuMinRadius = $state('0rem');
 
 	function toggleMenu() {
 		isMenuExpanded = !isMenuExpanded;
@@ -53,6 +54,7 @@
 				Math.max(centerX, window.innerWidth - centerX),
 				Math.max(centerY, window.innerHeight - centerY)
 			);
+			menuMinRadius = '5rem';
 		}
 	});
 
@@ -98,7 +100,7 @@
 	class="header__nav"
 	id="primary-nav"
 	data-visible={isMenuExpanded}
-	style={`--menu-origin-x: ${menuCenterX}px; --menu-origin-y: ${menuCenterY}px; --menu-radius: ${menuRadius}px;`}
+	style={`--menu-origin-x: ${menuCenterX}px; --menu-origin-y: ${menuCenterY}px; --menu-radius: ${menuRadius}px; --menu-min-radius: ${menuMinRadius};`}
 >
 	<ul role="list">
 		{#each navItems as item (item.href)}
@@ -171,6 +173,12 @@
 		inset: 0;
 		background: var(--color-gray-900);
 		color: var(--color-gray-100);
+
+		// Keep a small visible circle at the button center when collapsed.
+		// that's what menu-min-radius does
+		// Don't set menu-min-radius directly, set it in onMount, so we dont get a flash
+		// on page load
+		clip-path: circle(var(--menu-min-radius) at var(--menu-origin-x) var(--menu-origin-y));
 		opacity: 0;
 		visibility: hidden;
 		pointer-events: none;
@@ -201,9 +209,7 @@
 
 	.header__nav[data-visible='true'] {
 		// Expand to the computed viewport-covering radius.
-		clip-path: circle(
-			var(--menu-radius, '0px') at var(--menu-origin-x, '0px') var(--menu-origin-y, '0px')
-		);
+		clip-path: circle(var(--menu-radius) at var(--menu-origin-x) var(--menu-origin-y));
 		opacity: 1;
 		visibility: visible;
 		pointer-events: auto;
